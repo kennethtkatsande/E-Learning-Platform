2e77976135efe4644dfcd5a6a9fc8203a4413bae
kenneth <kennytakudzwa@gmail.com>
2026-07-18 13:33:33 +0200

    Creating models for polymorphic content

--- educa/courses/models.py
+++ educa/courses/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 
 # Create your models here.
 class Subject(models.Model):
@@ -45,4 +47,16 @@ class Module(models.Model):
     def __str__(self):
         return self.title
 
-    
+
+class Content(models.Model):
+    module = models.ForeignKey(
+    Module,
+    related_name='contents',
+    on_delete=models.CASCADE
+    )
+    content_type = models.ForeignKey(
+    ContentType,
+    on_delete=models.CASCADE
+    )
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey('content_type', 'object_id')
