e02d46863c9b1cdc0efce11f36d59b61ad6a6737
kenneth <kennytakudzwa@gmail.com>
2026-07-21 23:56:58 +0200

    Restricting access to class-based views, urls and templates

--- educa/educa/urls.py
+++ educa/educa/urls.py
@@ -18,7 +18,7 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
@@ -31,6 +31,7 @@ urlpatterns = [
         name='logout'
     ),
     path('admin/', admin.site.urls),
+    path('course/', include('courses.urls')),
 ]
 
 if settings.DEBUG:
