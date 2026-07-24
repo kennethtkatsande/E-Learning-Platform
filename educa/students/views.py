5e37fb3a04c775fd26c8d1ccb614c9264b1cc68b
kenneth <kennytakudzwa@gmail.com>
2026-07-24 20:02:10 +0200

    Enrolling in courses

--- educa/students/views.py
+++ educa/students/views.py
@@ -3,6 +3,9 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import FormView
+from .forms import CourseEnrollForm
 
 class StudentRegistrationView(CreateView):
     template_name = 'students/student/registration.html'
@@ -17,3 +20,18 @@ class StudentRegistrationView(CreateView):
         )
         login(self.request, user)
         return result
+
+
+class StudentEnrollCourseView(LoginRequiredMixin, FormView):
+    course = None
+    form_class = CourseEnrollForm
+
+    def form_valid(self, form):
+        self.course = form.cleaned_data['course']
+        self.course.students.add(self.request.user)
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy(
+        'student_course_detail', args=[self.course.id]
+        )
