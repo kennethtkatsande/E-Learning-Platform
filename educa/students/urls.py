092b34841c22c965ead0424726e04a95e94e220f
kenneth <kennytakudzwa@gmail.com>
2026-07-28 22:00:26 +0200

    Caching template fragments & Caching views & Using the per-site cache

--- educa/students/urls.py
+++ educa/students/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.views.decorators.cache import cache_page
 
 urlpatterns = [
     path(
@@ -25,6 +26,16 @@ urlpatterns = [
     path(
         'course/<pk>/<module_id>/',
         views.StudentCourseDetailView.as_view(),
+        name='student_course_detail_module'
+    ),
+    path(
+        'course/<pk>/',
+        cache_page(60 * 15)(views.StudentCourseDetailView.as_view()),
+        name='student_course_detail'
+    ),
+    path(
+        'course/<pk>/<module_id>/',
+        cache_page(60 * 15)(views.StudentCourseDetailView.as_view()),
         name='student_course_detail_module'
     ),
 ]
