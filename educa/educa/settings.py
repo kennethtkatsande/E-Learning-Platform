b1f030d8167b614b5c921c0bdb7d32bf2e7afbfa
kenneth <kennytakudzwa@gmail.com>
2026-07-28 19:31:21 +0200

    Using the cache framework & Adding Memcached to your project

--- educa/educa/settings.py
+++ educa/educa/settings.py
@@ -129,3 +129,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.PyMemcacheCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
