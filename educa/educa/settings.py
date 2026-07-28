7fdddea3f9f5abc520a589536572463f5bb1c8a5
kenneth <kennytakudzwa@gmail.com>
2026-07-28 21:42:09 +0200

    Checking cache requests with Django Debug Toolbar & Low-level caching based on dynamic data

--- educa/educa/settings.py
+++ educa/educa/settings.py
@@ -43,9 +43,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'embed_video',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -136,3 +138,7 @@ CACHES = {
         'LOCATION': '127.0.0.1:11211',
     }
 }
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
