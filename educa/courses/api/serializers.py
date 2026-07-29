15f6768d587c02b34855026796601d18fefd87fb
kenneth <kennytakudzwa@gmail.com>
2026-07-29 17:26:12 +0200

    Serializing course contents

--- educa/courses/api/serializers.py
+++ educa/courses/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from courses.models import Course, Module, Subject
+from courses.models import Content, Course, Module, Subject
 from django.db.models import Count
 
 class SubjectSerializer(serializers.ModelSerializer):
@@ -28,6 +28,44 @@ class ModuleSerializer(serializers.ModelSerializer):
 class CourseSerializer(serializers.ModelSerializer):
     modules = serializers.StringRelatedField(many=True, read_only=True)
     modules = ModuleSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Course
+        fields = [
+            'id',
+            'subject',
+            'title',
+            'slug',
+            'overview',
+            'created',
+            'owner',
+            'modules'
+        ]
+
+
+class ItemRelatedField(serializers.RelatedField):
+    def to_representation(self, value):
+        return value.render()
+
+
+class ContentSerializer(serializers.ModelSerializer):
+    item = ItemRelatedField(read_only=True)
+
+    class Meta:
+        model = Content
+        fields = ['order', 'item']
+
+
+class ModuleWithContentsSerializer(serializers.ModelSerializer):
+    contents = ContentSerializer(many=True)
+
+    class Meta:
+        model = Module
+        fields = ['order', 'title', 'description', 'contents']
+
+
+class CourseWithContentsSerializer(serializers.ModelSerializer):
+    modules = ModuleWithContentsSerializer(many=True)
 
     class Meta:
         model = Course
