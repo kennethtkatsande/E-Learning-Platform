a3aa5509fde13ea85ff63c5867b3a1649a6bf5e3
kenneth <kennytakudzwa@gmail.com>
2026-07-29 16:17:34 +0200

    Creating ViewSets and routers

--- educa/courses/api/views.py
+++ educa/courses/api/views.py
@@ -1,9 +1,11 @@
 from rest_framework import generics
-from courses.api.serializers import SubjectSerializer
-from courses.models import Subject
+from courses.api.serializers import CourseSerializer, SubjectSerializer
+from courses.models import Course, Subject
 from django.db.models import Count
 from courses.api.pagination import StandardPagination
+from rest_framework import viewsets
 
+"""
 class SubjectListView(generics.ListAPIView):
     queryset = Subject.objects.annotate(total_courses=Count('courses'))
     serializer_class = SubjectSerializer
@@ -13,3 +15,16 @@ class SubjectListView(generics.ListAPIView):
 class SubjectDetailView(generics.RetrieveAPIView):
     queryset = Subject.objects.annotate(total_courses=Count('courses'))
     serializer_class = SubjectSerializer
+
+"""
+
+class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Course.objects.prefetch_related('modules')
+    serializer_class = CourseSerializer
+    pagination_class = StandardPagination
+
+
+class SubjectViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Subject.objects.annotate(total_courses=Count('courses'))
+    serializer_class = SubjectSerializer
+    pagination_class = StandardPagination
